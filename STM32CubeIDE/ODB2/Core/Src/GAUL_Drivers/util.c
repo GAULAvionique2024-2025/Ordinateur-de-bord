bb262cdf369c32967c1cfbb07f60204f09907b56
bestrider14 <luka.dupuis.1@ulaval.ca>
2025-04-27 17:24:22 -0400

    Fixed printt()

--- STM32CubeIDE/ODB2/Core/Src/GAUL_Drivers/util.c
+++ STM32CubeIDE/ODB2/Core/Src/GAUL_Drivers/util.c
@@ -25,11 +25,13 @@ int printt(const char *format, ...) {
     va_list args;
     va_start(args, format);
     UpdateTime(&run_timer);
-    printf("[%03d:%02d:%03d] ",run_timer.elapsed_time_m, run_timer.elapsed_time_s, run_timer.elapsed_time_remaining_ms);
+    printf("[%03d:%02d:%03d]",run_timer.elapsed_time_m, run_timer.elapsed_time_s, run_timer.elapsed_time_remaining_ms);
 
     int ret = vprintf(format, args);
 
     va_end(args);
+
+    printf("\n");
     return ret;
 }
 
--- STM32CubeIDE/ODB2/Core/Src/main.c
+++ STM32CubeIDE/ODB2/Core/Src/main.c
@@ -125,7 +125,7 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  printt("Hello World");
-	  HAL_Delay(1000);
+	  HAL_Delay(200);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
