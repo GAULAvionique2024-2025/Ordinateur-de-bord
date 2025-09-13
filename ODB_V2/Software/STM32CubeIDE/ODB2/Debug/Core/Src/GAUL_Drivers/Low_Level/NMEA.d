298c227e1fc8a975f6d1d13ab3797868b8ec7ba7
Samuel Gagnon <118214762+SamLol12@users.noreply.github.com>
2025-09-12 23:24:42 -0400

    repo: Remove ODB_V1 and ODB_V2 from tracking

--- ODB_V2/Software/STM32CubeIDE/ODB2/Debug/Core/Src/GAUL_Drivers/Low_Level/NMEA.d
+++
@@ -1,4 +0,0 @@
-Core/Src/GAUL_Drivers/Low_Level/NMEA.o: \
- ../Core/Src/GAUL_Drivers/Low_Level/NMEA.c \
- ../Core/Inc/GAUL_Drivers/Low_Level/NMEA.h
-../Core/Inc/GAUL_Drivers/Low_Level/NMEA.h:
--- ODB_V2/Software/STM32CubeIDE/ODB2/Debug/Core/Src/GAUL_Drivers/Low_Level/RingBuffer.d
+++
@@ -1,4 +0,0 @@
-Core/Src/GAUL_Drivers/Low_Level/RingBuffer.o: \
- ../Core/Src/GAUL_Drivers/Low_Level/RingBuffer.c \
- ../Core/Inc/GAUL_Drivers/Low_Level/RingBuffer.h
-../Core/Inc/GAUL_Drivers/Low_Level/RingBuffer.h:
