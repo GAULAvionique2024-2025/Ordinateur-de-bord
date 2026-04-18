6e86578f78295541047bef6bff6828ae3ed39971
SamLol12 <gagnonsamuel12@gmail.com>
2026-04-18 13:35:58 -0400

    fix(rfd900x/mavlink): receiver/transmitter

--- RFD900x/receiver.py
+++ RFD900x/receiver.py
@@ -1,27 +1,27 @@
 import time
 from pymavlink import mavutil
 
-# Connection Ground Station (ID = 255)
+# 1. Connexion
 master = mavutil.mavlink_connection(
-    'com4',
-    baud=57600, 
-    source_system=255
+    'COM14', 
+    baud=115200, 
+    source_system=1
 )
-print("Ground Station Connected. Waiting for messages...")
+print("Ground Station lancée. Écoute des modules Air 1 et Air 2...")
 
 while True:
-    # Message
     msg = master.recv_match(blocking=False)
     if msg:
         msg_type = msg.get_type()
         if msg_type == 'STATUSTEXT':
             source_id = msg.get_srcSystem()
             texte_recu = msg.text.strip('\x00')
-            if source_id == 1:
-                print(f"🟢 [RECEIVED FROM AIR 1 - 30k ft] : {texte_recu}")
-            elif source_id == 2:
-                print(f"🔵 [RECEIVED FROM AIR 2 - 100k ft] : {texte_recu}")
+            
+            if source_id == 2:
+                print(f"🟢 [AIR 1 - 30k] : {texte_recu}")
+            elif source_id == 3:
+                print(f"🔵 [AIR 2 - 90k] : {texte_recu}")
             else:
-                print(f"⚪ [RECEIVED FROM UNKNOWN SOURCE {source_id}] : {texte_recu}")
+                print(f"⚪ [SOURCE {source_id}] : {texte_recu}")
 
     time.sleep(0.01)
--- RFD900x/transmitter.py
+++ RFD900x/transmitter.py
@@ -1,33 +1,19 @@
 import time
 from pymavlink import mavutil
 
-# Connection Air 1 (ID = 1)
 master = mavutil.mavlink_connection(
-    'com1', 
-    baud = 115200, 
-    source_system = 1
+    'COM15', 
+    baud=115200, 
+    source_system=2
 )
-master.wait_heartbeat()
-print("AIR 1 Connected. Beginning transmission...")
 
 counter = 0
-
 while True:
-    # Heartbeat
-    master.mav.heartbeat_send(
-        mavutil.mavlink.MAV_TYPE_ROCKET,
-        mavutil.mavlink.MAV_AUTOPILOT_INVALID,
-        0, 0, 0
-    )
+    master.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ROCKET, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
 
-    # Message
-    message = f"helloworld1 - msg {counter}".encode('utf-8')
-    master.mav.statustext_send(
-        mavutil.mavlink.MAV_SEVERITY_INFO,
-        message
-    )
+    msg_payload = f"Air1-30k: Status OK - {counter}"
+    master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, msg_payload.encode('utf-8'))
     
-    #print(f"Sended : {message.decode('utf-8')}")
+    print(f"Envoi Air 1: {msg_payload}")
     counter += 1
-    
     time.sleep(0.5)
