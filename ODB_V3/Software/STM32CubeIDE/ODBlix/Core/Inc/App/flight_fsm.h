069f8793a1836e13e8c02852c559d9bb1b477093
SamLol12 <118214762+SamLol12@users.noreply.github.com>
2026-05-04 00:36:06 -0400

    fix: add fsm function to his headerfile

--- ODB_V3/Software/STM32CubeIDE/ODBlix/Core/Inc/App/flight_fsm.h
+++ ODB_V3/Software/STM32CubeIDE/ODBlix/Core/Inc/App/flight_fsm.h
@@ -4,3 +4,11 @@
  *  Created on: 24 avr. 2026
  *      Author: gagno
  */
+
+#ifndef INC_APP_FLIGHT_FSM_H_
+#define INC_APP_FLIGHT_FSM_H_
+
+
+void FSM_Update(void);
+
+#endif /* INC_APP_FLIGHT_FSM_H_ */
